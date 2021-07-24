8cd679cd1f19753988e88760cb6e547969e59f75
dwalter0 <ds_walters@hotmail.com>
2021-07-25 06:25:54 +1000

    Added expects

--- challenge_higher_or_lower/src/main.rs
+++ challenge_higher_or_lower/src/main.rs
@@ -4,12 +4,12 @@ use std::io;
 fn main() {
    let number = thread_rng().gen_range(1..101);
    let mut guess: i32 = 0;
-   println!("A number has been generated. Guess the number that has been entered.");
+   println!("A number has been generated between 1 and 100. Guess the number that has been entered.");
    while guess != number {
         let mut buffer = String::new();
         //This will throw an error if an int isn't entered by the user. Error handling has not been covered yet.
-        io::stdin().read_line(&mut buffer);
-        guess = buffer.trim().parse().unwrap();
+        io::stdin().read_line(&mut buffer).expect("Failed to read standard in.");
+        guess = buffer.trim().parse().expect("Failed to parse the input value.");
         if guess < number {
             println!("Higher")
         } else if guess > number {
