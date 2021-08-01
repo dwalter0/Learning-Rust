80c56efa8b04646d999b039daa32d2cef30827eb
dwalter0 <ds_walters@hotmail.com>
2021-08-02 07:02:58 +1000

    post compile diff

--- challenge_average/target/debug/challenge_average.d
+++ challenge_average/target/debug/challenge_average.d
@@ -1,1 +1,1 @@
-C:\Users\ds_wa\OneDrive\Documents\Rust\challenge_average\target\debug\challenge_average.exe: C:\Users\ds_wa\OneDrive\Documents\Rust\challenge_average\src\main.rs
+C:\Users\ds_wa\OneDrive\Documents\Rust\Learning-Rust\challenge_average\target\debug\challenge_average.exe: C:\Users\ds_wa\OneDrive\Documents\Rust\Learning-Rust\challenge_average\src\main.rs
---
+++ challenge_word_count/target/debug/deps/challenge_word_count-0eac32572a17362d.d
@@ -0,0 +1,5 @@
+C:\Users\ds_wa\OneDrive\Documents\Rust\Learning-Rust\challenge_word_count\target\debug\deps\challenge_word_count-0eac32572a17362d.rmeta: src\main.rs
+
+C:\Users\ds_wa\OneDrive\Documents\Rust\Learning-Rust\challenge_word_count\target\debug\deps\challenge_word_count-0eac32572a17362d.d: src\main.rs
+
+src\main.rs:
